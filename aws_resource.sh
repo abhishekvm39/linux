8871e8654e0b309c26020cfd5f088b796c7b3a72
root <root@ip-172-31-45-66.eu-north-1.compute.internal>
2025-06-20 08:56:19 +0000

    file created

---
+++ aws_resource.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#
+#
+#Authoer : Abhishek V M 
+#Date : 20-june
+#
+#
+#Version v1
+#
+#
+set -x 
+
+
+#list s3 buckets 
+#
+echo "Print list of S3 buckets"
+
+aws s3 ls
+
+
+#list Ec2 instances 
+#
+echo "Print list of Ec2 instances"
+
+aws ec2 describe-instances
+
+
+#list lambda
+#
+#echo "print list of lambda functions"
+
+#aws lambda list-functions
+
+
+#list IAM users
+#
+#
+echo Print IAM users"
+
+
+aws iam list-users
+
+
+
